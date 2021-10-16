e55695a0cdca1bfb7177d7505c8f0887a485345f
brittanymbui <brittymbui@gmail.com>
2021-10-16 09:22:28 -0500

    Add login mutation to user resolver

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
+const { UserInputError } = require('apollo-server');
 const User = require('../../models/User');
 
 function generateToken(user) {
@@ -9,3 +10,12 @@ function generateToken(user) {
         name: user.name
     }, process.env.SECRET_KEY, { expiresIn: '6d' });
 };
+
+module.exports = {
+    Mutation: {
+        // LOGIN
+        async login(_, { email, password }) {
+            const {errors, valid}
+        }
+    }
+}
