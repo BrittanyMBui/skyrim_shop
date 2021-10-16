f3e9afee11d0f988f8157d11d2686adc056476e5
brittanymbui <brittymbui@gmail.com>
2021-10-16 08:57:56 -0500

    Add file structure, set up DB and server

---
+++ server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const connectionString = process.env.MONGODB_URI;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req })
+});
+
+mongoose
+    .connect(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('MongoDB connected');
+        return server.listen({ port: 4000 });
+    })
+    .then((res) => {
+        console.log(`Listening at ${res.url}`)
+    });
